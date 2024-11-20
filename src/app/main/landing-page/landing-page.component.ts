45e73501e82a460bb92cb29151f45f16e43ff990
Ufuk Özsahin <oezsahin95@icloud.com>
2024-11-20 11:08:04 +0100

    landingpage desktop version design finished

--- src/app/main/landing-page/landing-page.component.ts
+++ src/app/main/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 
 
 @Component({
@@ -15,6 +15,36 @@ export class LandingPageComponent {
 
   email = "info@ufuk-oezsahin.de";
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const scrollContent = this.el.nativeElement.querySelector('.scroll-content');
+    const scrollWrapper = this.el.nativeElement.querySelector('.scroll-wrapper');
+
+    if (scrollContent && scrollWrapper) {
+      const clonedContent = scrollContent.cloneNode(true);
+      this.renderer.appendChild(scrollWrapper, clonedContent);
+
+      const duration = 13; // Sekunden
+      this.renderer.setStyle(scrollContent, 'animation-duration', `${duration}s`);
+    }
+  }
+
+  // startAnimation(event: MouseEvent) {
+  //   const target = (event.target as HTMLElement).querySelector('p');
+  //   if (target) {
+  //     // Animation hinzufügen
+  //     this.renderer.setStyle(target, 'animation', 'text-slide 2s linear infinite');
+  //   }
+  // }
+
+  // stopAnimation(event: MouseEvent) {
+  //   const target = (event.target as HTMLElement).querySelector('p');
+  //   if (target) {
+  //     // Animation entfernen
+  //     this.renderer.removeStyle(target, 'animation');
+  //   }
+  // }
 
   setActiveLanguage(language: string): void {
     if (language === 'EN') {
