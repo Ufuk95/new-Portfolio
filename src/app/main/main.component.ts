5f94940ed149d43c630a896827307d6faac8260e
Ufuk Özsahin <oezsahin95@icloud.com>
2024-11-25 15:52:01 +0100

    about me break until new self image

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { CommonModule } from '@angular/common';
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LandingPageComponent, CommonModule],
+  imports: [LandingPageComponent, CommonModule, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
