7776c100617f273ae7fe446be525a6f531cad687
Ufuk Özsahin <oezsahin95@icloud.com>
2025-01-04 15:23:58 +0100

    animation change on landingpage and some translate options

--- src/app/main/landing-page/landing-page.component.ts
+++ src/app/main/landing-page/landing-page.component.ts
@@ -1,32 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
-
   email = "info@ufuk-oezsahin.de";
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit(): void {
-    const scrollContent = this.el.nativeElement.querySelector('.scroll-content');
-    const scrollWrapper = this.el.nativeElement.querySelector('.scroll-wrapper');
-
-    if (scrollContent && scrollWrapper) {
-      const clonedContent = scrollContent.cloneNode(true);
-      this.renderer.appendChild(scrollWrapper, clonedContent);
-
-      const duration = 13; // Sekunden
-      this.renderer.setStyle(scrollContent, 'animation-duration', `${duration}s`);
-    }
-  }
+  // Keine ngAfterViewInit-Methode mehr nötig
 }
